dc9246eac06e5c649e4cc2f440b6094a73c44a65
Bence Vass <v.bence.vass@gmail.com>
2020-06-26 10:26:13 +0200

    Docker update 0.9.4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/example.com/static/"
+STATIC_ROOT = "/var/www/backend/static/"
 
 GRAPHENE = {
     'SCHEMA': 'backend.schema.schema',
