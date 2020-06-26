16524b141ee5ad1207b2e00d9be2c346453e6934
Bence Vass <v.bence.vass@gmail.com>
2020-06-26 14:10:42 +0200

    Docker update 0.9.9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = "/var/www/backend/static/"
 
 GRAPHENE = {
