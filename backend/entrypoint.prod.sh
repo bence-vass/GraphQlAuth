d929e12888a0652854becaf679ad0caef9c8abec
Bence Vass <v.bence.vass@gmail.com>
2020-06-26 13:35:11 +0200

    Docker update 0.9.8.5

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -4,7 +4,7 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
 # python manage.py runserver 0.0.0.0:8000
-# gunicorn backend.wsgi:application --bind 0.0.0.0:8000
-gunicorn backend.wsgi -c gunicorn.conf
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+#gunicorn backend.wsgi -c gunicorn.conf
 
 exec "$@"
