6a0a0a2ac8100ee04330655028e84ae26fc82709
Bence Vass <v.bence.vass@gmail.com>
2020-09-04 08:12:19 +0200

    v1.2.2 backend fixes

---
+++ frontend/build/precache-manifest.960ae513f67576cc56c8eb44030fbece.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051998c5d1926f1f6f6f6c90c929fea5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1910fcdf8dcf245f13f",
+    "url": "/static/css/main.abb06b4e.chunk.css"
+  },
+  {
+    "revision": "136ee56309abd1609b28",
+    "url": "/static/js/2.d45a99d5.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.d45a99d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1910fcdf8dcf245f13f",
+    "url": "/static/js/main.857db292.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "e7a87bb62130829fdb1c8522c764cd36",
+    "url": "/static/media/apollo.e7a87bb6.svg"
+  },
+  {
+    "revision": "d715e9573ce7f99999ed86ab5612ebb2",
+    "url": "/static/media/apolloCaching.d715e957.png"
+  },
+  {
+    "revision": "ee7fbac9c0ca5b1dd6aef886bb695e63",
+    "url": "/static/media/apolloDiagram.ee7fbac9.svg"
+  },
+  {
+    "revision": "befc8dc820e6b15b879073f11401b3f9",
+    "url": "/static/media/certbot.befc8dc8.svg"
+  },
+  {
+    "revision": "b88b585481b1d1e5c322ca8bc29e591c",
+    "url": "/static/media/django.b88b5854.svg"
+  },
+  {
+    "revision": "ce63d4a1d50f749301699bf316409b3e",
+    "url": "/static/media/docker.ce63d4a1.svg"
+  },
+  {
+    "revision": "63f18e4b83bdbe5b9c68ee9cefe1a469",
+    "url": "/static/media/figill.63f18e4b.svg"
+  },
+  {
+    "revision": "45eecdbc4b119e5479617fa2e2cfff3d",
+    "url": "/static/media/graphene.45eecdbc.svg"
+  },
+  {
+    "revision": "81b66a4ff4187e507d44a3928faa6045",
+    "url": "/static/media/graphql.81b66a4f.svg"
+  },
+  {
+    "revision": "e78bab8e27dda8bb2ab5f51a84f224ce",
+    "url": "/static/media/graphqlquery.e78bab8e.png"
+  },
+  {
+    "revision": "b819a753fa8abff73c3bbf7cee967cf0",
+    "url": "/static/media/jwt.b819a753.svg"
+  },
+  {
+    "revision": "fc6d1dc88580d2900f1c85abb411243c",
+    "url": "/static/media/jwtFig.fc6d1dc8.svg"
+  },
+  {
+    "revision": "cd2acf553d65af9e80df0b7b8019e6e1",
+    "url": "/static/media/nginx.cd2acf55.svg"
+  },
+  {
+    "revision": "20223a72399ca6ddd4c9d70ffa212eca",
+    "url": "/static/media/queryexample.20223a72.png"
+  },
+  {
+    "revision": "9531e0390ad1f33048bad5439a95c2cc",
+    "url": "/static/media/react-grey.9531e039.svg"
+  },
+  {
+    "revision": "3d55b2d054e345074cce981ab01e1c95",
+    "url": "/static/media/text.3d55b2d0.svg"
+  },
+  {
+    "revision": "40bea301c3c1e196b4b28023a746dbc2",
+    "url": "/static/media/va-logo.40bea301.svg"
+  }
+]);
